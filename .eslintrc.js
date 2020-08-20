e91374a268d8928b503b032774d87cfe02d7eddd
Alver Grisales <Alver23@users.noreply.github.com>
2020-08-20 11:21:02 -0500

    feat: add configuration of the project (#2)
    - add environments folder
    - add structure base for project
    - add configuration for jest
    - add configuration for absolute path to imports

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,26 @@
 module.exports = {
+  env: {
+    es6: true,
+    node: true,
+    jest: true
+  },
   root: true,
   extends: '@react-native-community',
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
+  extends: [
+    'airbnb-base',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+    'plugin:react/recommended'
+  ],
+  rules: {
+    'import/no-unresolved': 'off',
+  },
+  "parserOptions": {
+    "ecmaFeatures": {
+      "jsx": true
+    }
+  }
 };
