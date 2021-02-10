0d0fbd0a505fe86d3090d146323717d7d0e19436
Alver Grisales <viga.23@hotmail.com>
2021-02-09 23:28:34 -0500

    feat: added loading component

---
+++ src/components/loading/__tests__/index.spec.tsx
@@ -0,0 +1,23 @@
+// Dependencies
+import React from 'react';
+import { shallow, ShallowWrapper } from 'enzyme';
+
+import Loading from './../index';
+
+describe('<Loading />', () => {
+  let component: ShallowWrapper;
+
+  const props = {};
+
+  beforeEach(() => {
+    component = shallow(<Loading {...props} />);
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('should save a snapshot of the component', () => {
+    expect(component).toMatchSnapshot();
+  });
+});
---
+++ src/components/loading/index.tsx
@@ -0,0 +1,22 @@
+// Dependencies
+import React, { ReactElement } from 'react';
+import { ActivityIndicator, View } from 'react-native';
+
+// Models
+import { ILoadingProps } from './models';
+
+// Styles
+import useStyles from './styles';
+
+const Loading = ({ loadingProps = {}, containerStyle }: ILoadingProps): ReactElement => {
+  const styles = useStyles();
+  const activiyProps = {
+    size: 'large',
+    color: styles.loading.color,
+    style: containerStyle,
+    ...loadingProps,
+  };
+  return <ActivityIndicator {...activiyProps} />;
+};
+
+export default Loading;
